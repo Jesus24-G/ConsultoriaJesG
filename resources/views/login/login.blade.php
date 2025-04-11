a297c80e98b9507eb26e05392af95656be129073
jes <jesgb18@gmail.com>
2025-04-10 19:12:43 -0600

    Cree el login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -8,56 +8,56 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    
+
 </head>
 
 <body>
 
     <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand logo" href="#">DEV GB</a>
+            <a class="navbar-brand logo" href="/">DEV GB</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link active" aria-current="page" href="/">Inicio</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <h2>Iniciar sesión como Administrador</h2>
 
-    <form method="POST" action="{{ route('admin.login') }}">
-        @csrf
-        <label>Email:</label>
-        <input type="email" name="email" required>
-        <br>
-        <label>Contraseña:</label>
-        <input type="password" name="password" required>
-        <br>
-        <button type="submit">Entrar</button>
-    </form>
+    <div class="col-md-5 mx-auto mt-5">
+        <form method="POST" action="" class="p-4 border shadow">
+        <h4 class="text-center">Login</h4>
+            @csrf
+            <div class="my-3">
+                <label class="form-label">Email:</label>
+                <input class="form-control" type="email" name="email" required>
+            </div>
 
-    @if($errors->any())
+            <div class="my-3">
+                <label class="form-label">Contraseña:</label>
+                <input class="form-control" type="password" name="password" required>
+            </div>
+
+            <div class="my-3 d-flex justify-content-end">
+                <input type="submit" class="btn btn-dark" value="Inciar Sesion">
+            </div>
+        </form>
+
+        @if($errors->any())
         <div style="color: red;">
             {{ $errors->first() }}
         </div>
-    @endif 
+        @endif
+    </div>
 
-        
+
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/admin/login', [LoginController::class, 'showLoginForm'])->name('admin.login.form');
+Route::get('/admin/login', [LoginController::class, 'showLoginForm'])->name('login.formulario');
 
